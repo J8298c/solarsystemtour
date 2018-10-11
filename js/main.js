bdc56d59152b3e0580a977f584e551577e9beb6f
julio <juliocmojica@gmail.com>
2018-10-10 22:19:50 -0400

    create planet function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let galaxy = document.getElementById('slidingcontainer');
 
-function createPlanets(height, width) {
+function createPlanets(height, width, color) {
   let planet = document.createElement('div');
   planet.style.height = height;
   planet.style.width = width;
+  planet.style.backgroundColor = color || 'white';
+  planet.style.border = `1px solid ${color || 'white'}`;
+  planet.style.borderRadius = '50%';
   return planet;
 }
 
